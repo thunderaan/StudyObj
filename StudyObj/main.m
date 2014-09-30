a5e9f9d4f171681cf8c5dcaa77b929385d8bdbd5
关 <guan@guandeMacBook-Pro.local>
2014-09-30 20:33:25 +0800

    Initial Commit

---
+++ StudyObj/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  StudyObj
+//
+//  Created by 关 on 14-9-30.
+//  Copyright (c) 2014年 关. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
