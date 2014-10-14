89b613b000c4d9d4469aa7ba4e5e3d43954b9df0
thunderaan <woxiangwohui@163.com>
2014-10-14 15:48:32 +0800

    1
    1

--- StudyObj/Card.h
+++ StudyObj/Card.h
@@ -1,1 +1,10 @@
-#import <Foundation/Foundation>1
+#import <Foundation/Foundation>
+
+@interface Card : NSObject
+
+@property (strong, nonatomic) NSString *contents;
+
+@property (nonatomic,getter=isChosen) BOOL chosen;
+@property (nonatomic,getter=isMatched) BOOL matched;
+
+-(int)match : (Card *)card;
